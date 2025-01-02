a833eb51a511b2e043771e7a2e11425803b3fc7a
cihanelibol <cihanelibol47@gmail.com>
2025-01-02 14:31:07 +0300

    the service that create hotel is scaffolded

--- Hotel.Infrastructure/obj/Debug/net8.0/Hotel.Infrastructure.AssemblyInfo.cs
+++ Hotel.Infrastructure/obj/Debug/net8.0/Hotel.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0e5e17bbd9f082855dfddcdb39d7937bd65f920")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b8eba076fe7f9b58be30eadcae6063f91deade")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
