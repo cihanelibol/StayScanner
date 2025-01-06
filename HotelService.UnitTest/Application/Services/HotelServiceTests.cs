b8b7c334570eb4d7491485fe712bc914722ce588
cihanelibol <cihanelibol47@gmail.com>
2025-01-06 12:51:51 +0300

    tests project added

---
+++ HotelService.UnitTest/Application/Services/HotelServiceTests.cs
@@ -0,0 +1,134 @@
+﻿using CosmosBase.Repository.Abstract;
+using Hotel.Application.Dto;
+using Hotel.Application.Services.Concrete;
+using Hotel.Domain.Entities;
+using Hotel.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.UnitTest.Application.Services
+{
+    public class HotelServiceTests
+    {
+        private readonly Mock<IUnitOfWork<HotelDbContext>> unitOfWorkMock;
+        private readonly HotelDbContext dbContext;
+        private readonly Hotel.Application.Services.Concrete.HotelService hotelService;
+
+
+        public HotelServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<HotelDbContext>()
+                .UseInMemoryDatabase(databaseName: "HotelTestDb")
+                .Options;
+
+            dbContext = new HotelDbContext(options);
+            unitOfWorkMock = new Mock<IUnitOfWork<HotelDbContext>>();
+            unitOfWorkMock.Setup(u => u.Context).Returns(dbContext);
+
+            hotelService = new HotelService(unitOfWorkMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateHotelAsync_Should_Create_Hotel_And_Return_Id()
+        {
+            var hotelDto = new HotelDto
+            {
+                CompanyName = "Test Hotel",
+                AuthorizedFirstName = "John",
+                AuthorizedLastName = "Doe"
+            };
+
+            var response = await hotelService.CreateHotelAsync(hotelDto);
+
+            Assert.True(response.IsSuccessful);
+            Assert.Equal((int)HttpStatusCode.Created, response.StatusCode);
+            Assert.NotNull(response.Data);
+        }
+
+        [Fact]
+        public async Task DeleteHotelAsync_Should_Set_IsDeleted_And_Return_True()
+        {
+            var hotel = new Hotel.Domain.Entities.Hotel
+            {
+
+                CompanyName = "Test Hotel",
+                AuthorizedFirstName = "Test Person",
+                AuthorizedLastName = "Test Person LastName",
+
+            };
+
+            dbContext.Hotels.Add(hotel);
+            await dbContext.SaveChangesAsync();
+
+            var response = await hotelService.DeleteHotelAsync(hotel.Id);
+
+            Assert.True(response.IsSuccessful);
+            Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
+            Assert.True((bool)response.Data);
+
+            var deletedHotel = dbContext.Hotels.SingleOrDefault(h => h.Id == hotel.Id);
+            Assert.NotNull(deletedHotel);
+            Assert.True(deletedHotel.IsDeleted);
+        }
+
+        [Fact]
+        public async Task GetAuthorizedByHotelIdAsync_Should_Return_AuthorizedPerson()
+        {
+            var hotel = new Hotel.Domain.Entities.Hotel
+            {
+                AuthorizedFirstName = "John",
+                AuthorizedLastName = "Doe",
+                CompanyName = "John Doe Hotel"
+            };
+
+            dbContext.Hotels.Add(hotel);
+            await dbContext.SaveChangesAsync();
+
+            var response = await hotelService.GetAuthorizedByHotelIdAsync(hotel.Id);
+
+            Assert.True(response.IsSuccessful);
+            Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
+
+            var authorizedPerson = response.Data as AuthorizedPerson;
+            Assert.NotNull(authorizedPerson);
+            Assert.Equal("John", authorizedPerson.AuthorizedFirstName);
+            Assert.Equal("Doe", authorizedPerson.AuthorizedLastName);
+        }
+        [Fact]
+        public async Task GetHotelsInfoByLocation_Should_Return_Hotel_Info_By_Location()
+        {
+            var hotel = new Hotel.Domain.Entities.Hotel
+            {
+                CompanyName = "John Doe Apart",
+                AuthorizedFirstName ="John",
+                AuthorizedLastName = "Doe",
+                Contacts = new List<Contact>
+                {
+                    new Contact { Location = "Yenişarbademli", PhoneNumber = "1234567890", Details = "5 Oda",Email = "crazyjohn@yahoo.com" }
+                }
+            };
+
+            dbContext.Hotels.Add(hotel);
+            await dbContext.SaveChangesAsync();
+
+            var response = await hotelService.GetHotelsInfoByLocation("Test Location");
+
+            Assert.True(response.IsSuccessful);
+            Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
+
+            var locationInfo = response.Data as GetHotelsInfoByLocationResponse;
+            Assert.NotNull(locationInfo);
+            Assert.Equal("Test Location", locationInfo.Location);
+            Assert.Equal(0, locationInfo.HotelCount);
+            Assert.Equal(0, locationInfo.PhoneNumberCount);
+        }
+
+
+    }
+}
---
+++ Report.UnitTest/Application/Services/ReportServiceTests.cs
@@ -0,0 +1,107 @@
+﻿using CosmosBase.Repository.Abstract;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Report.Application.Dtos;
+using Report.Application.Services.Abstract;
+using Report.Application.Services.Concrete;
+using Report.Domain.Enums;
+using Report.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report.UnitTest.Application.Services
+{
+    public class ReportServiceTests
+    {
+        private readonly Mock<IUnitOfWork<ReportDbContext>> unitOfWorkMock;
+        private readonly ReportDbContext dbContext;
+        private readonly ReportService reportService;
+
+        public ReportServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<ReportDbContext>()
+              .UseInMemoryDatabase(databaseName: "ReportTestDb")
+              .Options;
+
+            dbContext = new ReportDbContext(options);
+            unitOfWorkMock = new Mock<IUnitOfWork<ReportDbContext>>();
+            unitOfWorkMock.Setup(u => u.Context).Returns(dbContext);
+
+            reportService = new ReportService(null, unitOfWorkMock.Object);
+
+        }
+        [Fact]
+        public async Task CreateReportAsync_Should_Create_Report_And_Return_Id()
+        {
+            var createReportDto = new CreateReportDto
+            {
+                ReportDetail = "Test Report",
+                ReportType = ReportType.GetHotelsByLocation,
+                RequestedBody = ""
+            };
+
+            var response = await reportService.CreateReportAsync(createReportDto);
+
+            Assert.True(response.IsSuccessful);
+            Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(response.Data);
+
+        }
+        [Fact]
+        public async Task GetReportByIdAsync_Should_Return_Report()
+        {
+            var report = new Report.Domain.Entities.Report
+            {
+                ReportDetail = "Test Report",
+                ReportType = ReportType.GetHotelsByLocation
+            };
+
+            dbContext.Reports.Add(report);
+            await dbContext.SaveChangesAsync();
+
+            var response = await reportService.GetReportByIdAsync(report.Id);
+
+            Assert.True(response.IsSuccessful);
+            Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
+
+            var returnedReport = response.Data as Report.Domain.Entities.Report;
+            Assert.NotNull(returnedReport);
+            Assert.Equal(report.Id, returnedReport.Id);
+            Assert.Equal("Test Report", returnedReport.ReportDetail);
+        }
+
+        [Fact]
+        public async Task GetReportsAsync_Should_Return_Filtered_Reports()
+        {
+            var reports = new List<Report.Domain.Entities.Report>
+            {
+                new Report.Domain.Entities.Report
+                {
+                    ReportDetail = "Report 1",
+                    ReportStatus = ReportStatus.GettingReady,
+                    ReportType = ReportType.GetHotelsByLocation,
+                },
+                new Report.Domain.Entities.Report
+                {
+                    ReportDetail = "Report 2",
+                    ReportStatus = ReportStatus.Completed,
+                    ReportType = ReportType.GetHotelsByLocation,
+                }
+            };
+
+            dbContext.Reports.AddRange(reports);
+            await dbContext.SaveChangesAsync();
+
+            var response = await reportService.GetReportsAsync(ReportStatus.GettingReady, ReportType.GetHotelsByLocation);
+
+            Assert.True(response.IsSuccessful);
+            Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
+
+
+        }
+    }
+}
